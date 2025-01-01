e135572440b6db38e5c67c1946517939d1eea628
HaroldCris@Laptop <haroldcris@aitech.solutions>
2025-01-01 23:22:26 +0800

    Added delete script

--- stream.cmd
+++ stream.cmd
@@ -7,7 +7,7 @@
 setlocal enabledelayedexpansion
 
 :: Define the path to the .env file
-set ENV_FILE=path\to\.env
+set ENV_FILE=.env
 
 :: Read the .env file line by line
 for /f "usebackq tokens=* delims=" %%A in ("%ENV_FILE%") do (
@@ -46,5 +46,6 @@ set FOLDER=%~3
 :: Run the ffmpeg command with the specified IP address
 echo 'Recording from %IP% of %DEVICE% to %FOLDER%...'
 c:\ffmpeg\bin\ffmpeg -hide_banner -y -loglevel error -rtsp_transport tcp -use_wallclock_as_timestamps 1 -i rtsp://admin:%DEVICE_PASSWORD%@%IP%:554/stream1 -vcodec copy -acodec copy -f segment -reset_timestamps 1 -segment_time 300 -segment_format mkv -segment_atclocktime 1 -strftime 1 -strftime_mkdir 1 "%FOLDER%\\%DEVICE%\\%%Y-%%m-%%d\\%DEVICE%_%%Y-%%m-%%d_%%H.%%M.mkv"
+::c:\ffmpeg\bin\ffmpeg -hide_banner -y -loglevel error -rtsp_transport tcp -use_wallclock_as_timestamps 1 -i rtsp://admin:%DEVICE_PASSWORD%@%IP%:554/stream1 -vcodec copy -acodec copy -f hls -reset_timestamps 1 -hls_time 300 -segment_format mkv -segment_atclocktime 1 -atomic_writing 1 -strftime 1 -strftime_mkdir 1 -hls_segment_filename "%FOLDER%\\%DEVICE%\\%%Y-%%m-%%d\\%DEVICE%_%%Y-%%m-%%d_%%H.%%M.mkv" "%FOLDER%\\%DEVICE%\\%%Y-%%m-%%d\\%%Y-%%m-%%d_Playlist.m3u8"
 
 endlocal
